fb449cf23d1bbd2b0df72d1f2b235574d9318ff7
Rachel Fritz <rachelfritz0@gmail.com>
2020-10-13 13:55:07 -0500

    latest push

--- src/App.js
+++ src/App.js
@@ -1,59 +1,39 @@
-import React, { useState } from "react";
-import { capitalizeFirstLetter } from "../../utils/helpers";
+import React, { useState } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import About from './components/About';
+import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
-function Nav() {
-const [currentCategory, setCurrentCategory] = useState(categories[0]);
-const [categories] = useState([
-  {
-    name: 'commercial',
-    description: 'Photos of grocery stores, food trucks, and other commercial projects',
-  },
-  { name: 'portraits', description: 'Portraits of people in my life' },
-  { name: 'food', description: 'Delicious delicacies' },
-  { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-]);
+
+function App() {
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
-    <header className="flex-row px-1">
-      <h2>
-        <a data-testid="link" href="/">
-          <span role="img" aria-label="camera">
-            {" "}
-            📸
-          </span>{" "}
-          Oh Snap!
-        </a>
-      </h2>
-      <nav>
-        <ul className="flex-row">
-          <li className="mx-2">
-            <a
-              href="#about"
-            >
-              About me
-            </a>
-          </li>
-          <li>
-            <span>Contact</span>
-          </li>
-          {categories.map((category) => (
-            <li className={`mx-1 ${
-                currentCategory.name === category.name && 'navActive'
-                }`} key={category.name}>
-              <span
-                onClick={() => {
-                  setCurrentCategory(category)
-                }}
-              >
-                {capitalizeFirstLetter(category.name)}
-              </span>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
+      <main>
+        <div>
+          <Gallery></Gallery>
+          <About></About>
+        </div>
+      </main>
+    </div>
   );
 }
 
-export default Nav;
-
+export default App;
